838ee549a918d8748953801db3f1677f6c2ba9d6
sylarBrest <aostrovsky87@gmail.com>
2022-01-24 15:17:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,33 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
 
+const MORSE_SYMBOLS = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+    '**': ' ',
+}
+
 function decode(expr) {
-    // write your solution here
+    let res = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        res.push(expr.slice(i, i + 10));
+    }
+    res = res.map(elem => {
+                 let val = [];
+                 for (let i = 0; i < elem.length; i += 2) {
+                     val.push(elem.slice(i, i + 2));
+                 }
+                 return val;
+             })
+             .map(el => el.map(val => MORSE_SYMBOLS[val]))
+             .map(el => el.join(''))
+             .map(el => MORSE_TABLE[el])
+             .join('');
+    return res;
 }
 
 module.exports = {
